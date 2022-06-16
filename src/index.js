ee666ec8079f1a671acf55b3a8fdf9cd6b5001d4
lmigonzalez <lmigonzalezs@gmail.com>
2022-06-15 23:54:11 -0400

    context setup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { StateContext } from './context/StateContext';
 import {BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <StateContext>
+        <App />
+      </StateContext>
     </React.StrictMode>
   </BrowserRouter>
 );
