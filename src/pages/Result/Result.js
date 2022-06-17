88aada1c5733421a0f092c193df0810f9c10c6d5
lmigonzalez <lmigonzalezs@gmail.com>
2022-06-17 00:10:35 -0400

    result tablet show the correct data

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -1,90 +1,128 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Result.css";
 import { useNavigate } from "react-router-dom";
-import { BsCheckLg } from "react-icons/bs";
+import { BsCheckLg, BsXCircleFill } from "react-icons/bs";
 import { Button } from "react-bootstrap";
-import ResultAnswer from "../../components/ResultAnswer";
+
 
 import { useStateContext } from "../../context/StateContext";
 
 function Result() {
-	const { resultArray, setResultArray } = useStateContext();
-	const navigate = useNavigate()
+  const { resultArray, setResultArray } = useStateContext();
+  const navigate = useNavigate();
 
-	const handleExit = () =>{
-		navigate('/')
-	}
-	const handleTryAgain = () =>{
-		navigate('/selectdifficulty')
-	}
+  const [correctAnswers, setCorrectAnswers] = useState(0)
+  const [resultPoints, setResultPoints] = useState(0)
+  let num = 0
 
-	console.log(resultArray)
+// let correctAnswers = 0
+// let resultPoints = 0
+
+  useEffect(() =>{
+	  getPoints()
+  }, [])
+
+  const handleExit = () => {
+    navigate("/");
+  };
+  const handleTryAgain = () => {
+    navigate("/selectdifficulty");
+  };
+
+  console.log(resultArray);
+
+  const getPoints = () =>{
+	  for(let data of resultArray){
+		  if(data.yourAnswer === data.correctAnswer){
+			
+			num ++
+			  
+		  }
+	  }
+
+	  setCorrectAnswers(num)
+	  setResultPoints(num *  5)
+  }
 
   return (
     <section className="result-container">
       <div className="result-content">
         <div className="result-details">
-          <p className="correct-result">17 from 20</p>
-          <p>Points: 90</p>
+          <p className="correct-result">{`${correctAnswers} from ${resultArray.length}`}</p>
+          <p>{`Points: ${resultPoints}`}</p>
         </div>
         <table className="result-table">
           <thead>
             <tr>
               <th>Result</th>
               <th>Country</th>
-              <th>Correct Answer</th>
               <th>Your Answer</th>
+              <th>Correct Answer</th>
             </tr>
           </thead>
 
           <tbody>
-
-            <tr>
-              <td>
-                <BsCheckLg />
-              </td>
-              <td> Cuba </td>
-              <td> Havana </td>
-              <td> Tokio </td>
-            </tr>
-
-			<tr>
-              <td>
-                <BsCheckLg />
-              </td>
-              <td> France </td>
-              <td> Paris </td>
-              <td> Rome </td>
-            </tr>
-
-			<tr>
-              <td>
-                <BsCheckLg />
-              </td>
-              <td> France </td>
-              <td> Paris </td>
-              <td> Rome </td>
-            </tr>
-
-			<tr>
-              <td>
-                <BsCheckLg />
-              </td>
-              <td> France </td>
-              <td> Paris </td>
-              <td> Rome </td>
-            </tr>
-
+            {resultArray.map((data) => {
+              return (
+                <tr>
+                  <td>
+					  {data.yourAnswer === data.correctAnswer ? <BsCheckLg className="correct-icon"/> : <BsXCircleFill className="incorrect-icon"/>}
+                    
+                  </td>
+                  <td> {data.country} </td>
+                  <td> {data.yourAnswer} </td>
+                  <td> {data.correctAnswer} </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
 
-		<div className="result-buttons">
-			<Button onClick={handleExit}>Exit</Button>
-			<Button onClick={handleTryAgain}>Try Again</Button>
-		</div>
+        <div className="result-buttons">
+          <Button onClick={handleExit}>Exit</Button>
+          <Button onClick={handleTryAgain}>Try Again</Button>
+        </div>
       </div>
     </section>
   );
 }
 
 export default Result;
+
+{
+  /* <tr>
+<td>
+  <BsCheckLg />
+</td>
+<td> Cuba </td>
+<td> Havana </td>
+<td> Tokio </td>
+</tr>
+
+<tr>
+<td>
+  <BsCheckLg />
+</td>
+<td> France </td>
+<td> Paris </td>
+<td> Rome </td>
+</tr>
+
+<tr>
+<td>
+  <BsCheckLg />
+</td>
+<td> France </td>
+<td> Paris </td>
+<td> Rome </td>
+</tr>
+
+<tr>
+<td>
+  <BsCheckLg />
+</td>
+<td> France </td>
+<td> Paris </td>
+<td> Rome </td>
+</tr> */
+}
