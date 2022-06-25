36c21d98c958ce5b3bd425e3d6b6ee582be1498d
lmigonzalez <lmigonzalezs@gmail.com>
2022-06-25 14:09:21 -0400

    login form validation

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
@@ -20,55 +20,80 @@ function Login() {
 		setGamesPlayed,
 		setCorrectAnswers,
   } = useStateContext();
-
+  
   const initialData = {
     email: "",
     password: "",
   };
 
   const [userData, setUserData] = useState(initialData);
+  const [submitDisabled, setSubmitDisabled] = useState(true)
 
-  const handleChange = (e) => {
-    setUserData({
-      ...userData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  useEffect(()=>{
+    checkIfInputFields()
+  }, [userData])
 
-  const handleCancel = () => {
-    navigate("/");
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
+
+  const checkIfInputFields = () =>{
+    if(userData.email.length < 3 || userData.password.length < 3){
+  
+      setSubmitDisabled(true)
+    }else{
+      setSubmitDisabled(false)
+      
+    }
+  }
+
+
+    
+    const handleChange = (e) => {
+      setUserData({
+        ...userData,
+        [e.target.name]: e.target.value,
+      });
+      
+   
+
+   
     };
+    
+    const handleCancel = () => {
+      navigate("/");
+    };
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      
+      const data = JSON.stringify(userData);
+      
+      axios.post('http://localhost:3000/api/login', data, config)
+      .then((res)=>{
+        // console.log(res.data)
+        setToken(res.data.token)
+        setUserId(res.data.user._id)
+        setName(res.data.user.name)
+        setUserPoints(res.data.user.points)
+        setGamesPlayed(res.data.user.gamesPlayed)
+        setCorrectAnswers(res.data.user.correctAnswers)
+        localStorage.setItem('userData', JSON.stringify({userId: res.data.user._id, token: res.data.token}))
+        
+        navigate('/')
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+      
+    };
+    // console.log(userData.email.length)
 
-    const data = JSON.stringify(userData);
-
-    axios.post('http://localhost:3000/api/login', data, config)
-    .then((res)=>{
-      // console.log(res.data)
-      setToken(res.data.token)
-      setUserId(res.data.user._id)
-      setName(res.data.user.name)
-      setUserPoints(res.data.user.points)
-		  setGamesPlayed(res.data.user.gamesPlayed)
-		  setCorrectAnswers(res.data.user.correctAnswers)
-      localStorage.setItem('userData', JSON.stringify({userId: res.data.user._id, token: res.data.token}))
-
-      navigate('/')
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-
-  };
-
-  return (
+    
+    return (
     <section className="form-container">
       <div className="form">
         <h1>Login</h1>
@@ -96,7 +121,7 @@ function Login() {
           </Form.Group>
 
           <div className="button-group">
-            <Button type="submit">Submit</Button>
+            <Button type="submit" disabled={submitDisabled} >Submit</Button>
             <Button onClick={handleCancel}>Cancel</Button>
           </div>
         </Form>
